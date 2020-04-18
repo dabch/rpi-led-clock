5eefb030253b82806d2f6d98fcdb92dbca714c5f
Daniel Bücheler <dabch@posteo.de>
2020-04-18 12:37:32 +0100

    add experimental code to display EUR-JPY exchange rate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 extern crate image;
 
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+
 use image::{GenericImage, ImageBuffer, RgbImage, DynamicImage};
 
 use rust_rpi_led_matrix as m;
 use std::{thread, time};
+use std::path::Path;
 
-fn main() {
+#[derive(Serialize, Deserialize, Debug)]
+struct APIRes {
+    amount: f64,
+    base: String,
+    date: String,
+    rates: HashMap<String, f64>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
 
     let options = m::LedMatrixOptions::new();
@@ -13,53 +25,44 @@ fn main() {
     let matrix = m::LedMatrix::new(options);
     let canvas = matrix.offscreen_canvas();
 
-    let image = image::open("./sun-yellow.png").unwrap();
+    let image = image::open("../../sun-red.png").unwrap();
+    let image2 = image::open("sun-yellow.png").unwrap();
 
     //let pixel = image.get_pixel(0, 0);
     //let pixel = image.get_pixel(0, 0);
 
     //println!("image: {:?}", image);
+    //
+    //canvas.show_image(&image, 32, 0);
+    //canvas.show_image(&image2, 0, 0);
 
-    let rgbi = match image {
-        //DynamicImage::ImageRgb8(i) => { println!("rgb8"); i },
-        DynamicImage::ImageRgba8(i) => { println!("rgba8"); i },
-        _ => { println!("other"); image::ImageBuffer::new(32, 32) },
-    };
-
-    for y in 0..32 {
-        for x in 0..32 {
-
-            let px = rgbi.get_pixel(x,y);//.get(0);
-
-            let image::Rgba([r, g, b, a]) = px; 
-
-            //print!("{:?},", px);
-            //print!("{}", if px[3] > 100 { "+ " } else { "  " });
-            let brightness = (*a as u32 * 100 >> 8) as u8;
-            if brightness > 5 {
-                matrix.set_brightness(brightness);
-                canvas.set_pixel(x as i32, y as i32, *r, *g, *b);
-            } 
-            println!("{} -> {}", px[3], (px[3] as u32 * 100 / 256) as u8);
-        }
-        println!();
-    }
-
-    /*{
-        DynamicImage::ImageRgb8(i) => { println!("rgb8")},
-        DynamicImage::ImageRgba8(i) => { println!("rgba8")},
-        DynamicImage::ImageRgb16(i) => { println!("rgb16")},
-        DynamicImage::ImageRgba16(i) => { println!("rgba16")},
-        _ => { println!("other") },
-    }*/
-    //canvas.draw_line(0, 0, 63, 31, 255, 255, 255);
-    //matrix.set_brightness(10);
-    //thread::sleep(time::Duration::from_millis(1000));
-    //canvas.draw_line(0, 31, 63, 0, 255, 255, 255);
-    let canvas = matrix.swap(canvas);
-    thread::sleep(time::Duration::from_millis(10000));
+    //let canvas = matrix.swap(canvas);
+    //thread::sleep(time::Duration::from_millis(10000));
 
     //canvas.draw_circle(0, 0, 15, 255, 255, 255);
     //let canvas = matrix.swap(canvas);
     //thread::sleep(time::Duration::from_millis(5000));
+
+    let resp = reqwest::blocking::get("https://api.frankfurter.app/latest?from=EUR")?
+        .json::<APIRes>()?;
+
+    println!("{:?}", resp);
+
+    let yen = match resp.rates.get("JPY") {
+        Some(n) => *n,
+        None => -1.0
+    };
+
+    println!("EUR 1 = JPY {:}", yen);
+
+    let f = m::LedFont::new(Path::new("/home/pi/rpi-rgb-led-matrix/fonts/5x7.bdf"));
+
+    canvas.draw_text(f, 6, 6, 255, 255, 255, format!("¥{}", yen), 0);
+    //canvas.vertical_draw_text(f, 16, 16, 255, 255, 255, "Test".to_string(), 0);
+    
+    let canvas = matrix.swap(canvas);
+    thread::sleep(time::Duration::from_millis(10000));
+    Ok(())
 }
+
+
